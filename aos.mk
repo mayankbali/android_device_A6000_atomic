0f379bd3459d79c0557f4efc77cc2bee7e3e8b6e
mayankbali <mayankbali3@gmail.com>
2017-09-05 16:42:47 +0530

    santoni:atomic bringup

--- aos.mk
+++ aos.mk
@@ -16,10 +16,10 @@
 
 $(call inherit-product, device/xiaomi/santoni/full_santoni.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common AtomicOS stuff.
+$(call inherit-product, vendor/aos/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_santoni
+PRODUCT_NAME := aos_santoni
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
@@ -27,3 +27,5 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT="Xiaomi/santoni/santoni:7.1.2/N2G47H/7.7.6:user/release-keys" \
     PRIVATE_BUILD_DESC="santoni-user 7.1.2 N2G47H 7.7.6 release-keys"
+
+WITH_ROOT := true
